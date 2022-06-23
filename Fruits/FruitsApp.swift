0aca1354ccce237e833fd81e919da4f400740857
hiusanmaz <hiusanmaz34@gmail.com>
2022-06-23 21:52:22 +0300

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Halil Usanmaz on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
