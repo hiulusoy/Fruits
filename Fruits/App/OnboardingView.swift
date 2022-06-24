e14b5e8c5adc7727ed94fcce4050f93dd028fc47
hiusanmaz <hiusanmaz34@gmail.com>
2022-06-24 08:36:14 +0300

    OnBoardingScreen added

---
+++ Fruits/App/OnboardingView.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingView.swift
+//  Fruits
+//
+//  Created by Halil Usanmaz on 24.06.2022.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
+    
+    
+    var body: some View {
+        TabView{
+            ForEach(0..<5){
+                item in
+                FruitCardView()
+            }
+    } //:TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
