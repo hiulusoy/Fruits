b9915beec9a6f1ea46b18fdd5acc9742eb9ae943
hiusanmaz <hiusanmaz34@gmail.com>
2022-06-24 19:26:38 +0300

    Onboarding screen done && data && models added

---
+++ Fruits/Model/FruitModel.swift
@@ -0,0 +1,23 @@
+//
+//  FruitModel.swift
+//  Fruits
+//
+//  Created by Halil Usanmaz on 24.06.2022.
+//
+
+import SwiftUI
+
+// MARK: FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID();
+    var title: String;
+    var headline: String;
+    var image: String;
+    var gradientColors: [Color];
+    var description: String;
+    var nutrition: [String];
+    
+    
+
+}
